1649ab1a3506ccef79e5e5381dd9a3029fc3ef9a
John William <johnwill.v.2017@gmail.com>
2022-03-05 12:30:16 -0300

    add field arquivo in class cotacao

--- back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/model/Cotacao.java
+++ back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/model/Cotacao.java
@@ -1,5 +1,9 @@
 package br.com.remsoft.equipe3.hackathon.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -8,11 +12,14 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "cotacao")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cotacao {
     private String tituloCotação;
     private String descricao;
     private LocalDate dataLimiteResposta;
     @OneToOne
     private Empresa empresa;
-//    private  lista;
+    private String arquivo;
 }
