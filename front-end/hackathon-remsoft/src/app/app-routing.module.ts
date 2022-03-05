14f17877eb3299e0255d3cee0914d0cbec3df69c
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-05 12:37:17 -0300

    adicionado navbar, login, comprador e fornecedor components

--- front-end/hackathon-remsoft/src/app/app-routing.module.ts
+++ front-end/hackathon-remsoft/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompradorComponent } from './pages/comprador/comprador.component';
+import { FornecedorComponent } from './pages/fornecedor/fornecedor.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'comprador',
+    component: CompradorComponent
+  },
+  {
+    path: 'fornecedor',
+    component: FornecedorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/hackathon-remsoft/src/app/app.module.ts
+++ front-end/hackathon-remsoft/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { FornecedorComponent } from './pages/fornecedor/fornecedor.component';
+import { CompradorComponent } from './pages/comprador/comprador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    FornecedorComponent,
+    CompradorComponent
   ],
   imports: [
     BrowserModule,
