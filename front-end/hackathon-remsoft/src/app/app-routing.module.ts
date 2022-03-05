90393eef946ed19a0bb8db9d0ce587fe47e6c785
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-05 15:14:59 -0300

    adicionado layout na pagina comprador && adiciona nova-cotacao e respostas-fornecedores components

--- front-end/hackathon-remsoft/src/app/app-routing.module.ts
+++ front-end/hackathon-remsoft/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { CompradorComponent } from './pages/comprador/comprador.component';
 import { FornecedorComponent } from './pages/fornecedor/fornecedor.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NovaCotacaoComponent } from './pages/nova-cotacao/nova-cotacao.component';
+import { RespostasFornecedoresComponent } from './pages/respostas-fornecedores/respostas-fornecedores.component';
 
 const routes: Routes = [
   {
@@ -21,6 +23,14 @@ const routes: Routes = [
   {
     path: 'fornecedor',
     component: FornecedorComponent
+  },
+  {
+    path: 'comprador/nova-cotacao',
+    component: NovaCotacaoComponent
+  },
+  {
+    path: 'comprador/respostas-fornecedores',
+    component: RespostasFornecedoresComponent
   }
 ];
 
