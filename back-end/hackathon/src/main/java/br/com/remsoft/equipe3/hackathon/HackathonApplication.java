458ab305a1818ec34cdc3a466db66b1bf85521d2
John William <johnwill.v.2017@gmail.com>
2022-03-05 10:43:46 -0300

    commit init

---
+++ back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/HackathonApplication.java
@@ -0,0 +1,13 @@
+package br.com.remsoft.equipe3.hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonApplication.class, args);
+	}
+
+}
---
+++ back-end/hackathon/src/test/java/br/com/remsoft/equipe3/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.remsoft.equipe3.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
