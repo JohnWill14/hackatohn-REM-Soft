ff716c6b21efc9e05bcf475b9bd19ca697f33f59
John William <johnwill.v.2017@gmail.com>
2022-03-05 12:40:47 -0300

    conectando banco

--- back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/model/Cotacao.java
+++ back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/model/Cotacao.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.FileInputStream;
 import java.time.LocalDate;
 
@@ -16,6 +14,8 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Cotacao {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String tituloCotação;
     private String descricao;
     private LocalDate dataLimiteResposta;
