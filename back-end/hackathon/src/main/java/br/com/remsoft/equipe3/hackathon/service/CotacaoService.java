41939323fb7da3bdc312f961a196d49510c98a5d
John William <johnwill.v.2017@gmail.com>
2022-03-05 13:04:15 -0300

    add services

--- back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/service/CotacaoService.java
+++ back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/service/CotacaoService.java
@@ -1,2 +1,30 @@
-package br.com.remsoft.equipe3.hackathon.service;public class CotacaoService {
+package br.com.remsoft.equipe3.hackathon.service;
+
+import br.com.remsoft.equipe3.hackathon.model.Cotacao;
+import br.com.remsoft.equipe3.hackathon.model.Empresa;
+import br.com.remsoft.equipe3.hackathon.repository.CotacaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CotacaoService {
+    private final CotacaoRepository cotacaoRepository;
+
+    public Cotacao add(Cotacao cotacao){
+        return cotacaoRepository.save(cotacao);
+    }
+
+    public List<Cotacao> findAll(){
+        return cotacaoRepository.findAll();
+    }
+
+    public List<Cotacao> findAllByIdEmpresa(Long id){
+        return cotacaoRepository.findAll().stream()
+                .filter(c -> c.getEmpresa().getId().equals(id))
+                .collect(Collectors.toList());
+    }
 }
--- back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/service/EmpresaService.java
+++ back-end/hackathon/src/main/java/br/com/remsoft/equipe3/hackathon/service/EmpresaService.java
@@ -1,2 +1,28 @@
-package br.com.remsoft.equipe3.hackathon.service;public class EmpresaService {
+package br.com.remsoft.equipe3.hackathon.service;
+
+import br.com.remsoft.equipe3.hackathon.model.Empresa;
+import br.com.remsoft.equipe3.hackathon.repository.EmpresaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmpresaService {
+    private final EmpresaRepository empresaRepository;
+
+    public List<Empresa> findAll(Empresa empresa){
+        return empresaRepository.findAll();
+    }
+
+    public Empresa findByIdOrThrowException(Long id){
+        Optional<Empresa> byId = empresaRepository.findById(id);
+        return byId.orElseThrow(()-> new RuntimeException("id nao encontrado"));
+    }
+
+    public Empresa add(Empresa empresa){
+        return empresaRepository.save(empresa);
+    }
 }
